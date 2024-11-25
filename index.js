104d3bd05a532fd16961876b11677228a434f543
Ibrahim Sarkar <ibrahim06978@gmail.com>
2024-11-25 12:29:28 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-const jsonServer = require("json-server"); // Import json-server
-const cors = require("cors"); // Import cors for handling cross-origin requests
-
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
-// Middleware to parse JSON
-server.use(jsonServer.bodyParser);
-
-// Enable CORS
-server.use(cors());
-
-// Use default middlewares (e.g., static files, logging)
 server.use(middlewares);
-
-// Use the router
 server.use(router);
 
-// Start the server
-server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`);
-});
+server.listen(port);
